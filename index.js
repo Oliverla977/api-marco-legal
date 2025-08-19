290d8a3ca4c55100fd42a80fd7e439f591b51889
OliverGT22 <109478445+OliverGT22@users.noreply.github.com>
2025-08-18 21:25:52 -0600

    correccion url

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(cors({
   //origin: 'http://localhost:4200'
-  origin: 'https://cumplimiento-marcos-legales.web.app/'
+  origin: 'https://cumplimiento-marcos-legales.web.app'
 }));
 
 // Rutas de api
